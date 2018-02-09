a4ad90367a7f9714f427081020cfb4117f8ae984
Jason Dudash <thenut77@gmail.com>
2018-02-09 15:28:25 -0500

    Update 99-launch.sh

--- 99-launch.sh
+++ 99-launch.sh
@@ -1,6 +1,8 @@
+echo found the following configs:
+ls -al /startup-sequence/configs/
 echo using datadir of $DATADIR
 cp /startup-sequence/configs/znc.conf $DATADIR/configs
 echo DATADIR CONFIGS ARE
-ls $DATADIR/configs
+ls -al $DATADIR/configs/
 cat $DATADIR/configs/znc.conf
 /opt/znc/bin/znc --foreground --datadir "$DATADIR" "$@"
