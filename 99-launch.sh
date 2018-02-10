732561fe70ac735d1080b8f209d11b1d664391f1
Jason Dudash <thenut77@gmail.com>
2018-02-09 19:16:59 -0500

    Update 99-launch.sh

--- 99-launch.sh
+++ 99-launch.sh
@@ -1,8 +1,13 @@
-DATADIR=${OVERRIDE_DATADIR:-$DATADIR}
-echo using datadir of $DATADIR
+# copy startup config to default data dir
 mkdir -p $DATADIR/configs
 cp /startup-sequence/configs/znc.conf $DATADIR/configs
-echo DATADIR CONFIGS ARE
-ls -al $DATADIR/configs/
+
+# allow for override of the datadir
+DATADIR=${OVERRIDE_DATADIR:-$DATADIR}
+
+echo using datadir of $DATADIR
+echo DATADIR CONFIG IS
 cat $DATADIR/configs/znc.conf
+
+#run the ZNC app
 /opt/znc/bin/znc --foreground --datadir "$DATADIR" "$@"
